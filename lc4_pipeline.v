571553800a9832f4075f95caa95c3301469b5d69
Archit Sharma <architsh@seas.upenn.edu>
2018-03-22 20:28:18 -0400

    x

--- lc4_pipeline.v
+++ lc4_pipeline.v
@@ -36,7 +36,138 @@ module lc4_processor
    /*** YOUR CODE HERE ***/
 
 
-  
+    // execute (s stage)
+
+
+    //x register out wires
+    wire [15:0] x_pc;
+    wire [15:0] x_insn;
+    wire [15:0] x_r1out;
+    wire [15:0] x_r2out;
+    wire [15:0] x_sext;
+    wire [2:0] x_r1sel;
+    wire [2:0] x_r2sel;
+    wire [2:0] x_wsel;
+    wire x_alu_src;
+    wire x_regfile_we;
+    wire x_nzp_we;
+    wire x_is_store;
+    wire x_is_load;
+    wire x_is_branch;
+    wire x_is_control_insn;
+    wire [1:0] x_stall;
+    wire x_select_pc_plus_one;
+    wire [15:0] x_pc_plus_one;
+
+    // m input wires;
+    wire [15:0] x_new_insn;
+    wire [15:0] o_result;
+    wire [15:0] x_r2out;
+
+    //additional wires
+    wire [15:0] i_wdata;
+
+    assign x_new_insn = x_insn;
+    
+
+
+    /**......................wires from w..........................**/
+
+    //w_regfile_we
+    //w_wsel
+
+    /**......................wires from w..........................**/
+
+    //m_sel
+    //m_refile_we
+
+
+
+
+    // test outputs
+    //assign test_dmem_we = o_dmem_we;
+    //assign test_dmem_addr = o_dmem_addr;
+
+    //assign test_regfile_data = i_wdata;
+    assign i_wdata = w_rd_data;
+
+
+    // NZP output
+    wire [2:0] nzp_out;
+    // NZP
+    //assign test_nzp_new_bits = nzp_out;
+
+    nzp n0 (x_nzp_we, i_wdata, nzp_out, clk, gwe, rst, test_nzp_new_bits);
+
+    
+    // branch output
+    wire [15:0] x_next_pc;
+
+
+    // branch     
+    branch b0 (.pc_plus_one(pc_plus_one), .i_cur_insn(x_insn), .nzp_out(nzp_out), .o_result(o_result), .is_branch(x_is_branch), .is_control_insn(x_is_control_insn), .next_pc(x_next_pc));
+
+
+    //x bypass logic
+
+    wire [1:0] x_bypass_logic1;
+    wire [1:0] x_bypass_logic2;
+
+    x_bypass_l b0 (.x_rsel(x_r1sel), .m_wsel(m_wsel), .w_wsel(w_wsel), .m_regfile_we(m_regfile_we), .w_regfile_we(w_regfile_we), .x_bypass_logic(x_bypass_logic1));
+    x_bypass_l b0 (.x_rsel(x_r2sel), .m_wsel(m_wsel), .w_wsel(w_wsel), .m_regfile_we(m_regfile_we), .w_regfile_we(w_regfile_we), .x_bypass_logic(x_bypass_logic2));
+
+
+
+    //after by mux
+
+
+    wire [15:0] alu_in1;
+    wire [15:0] alu_in2;
+
+    wire [15:0] sign_ext;
+
+    assign sign_ext = (x_alu_out == 1'b0) ? x_r2out : x_insn;
+    ////////////^^ could be d_insn
+
+    assign alu_in1 = (x_bypass_logic1 == 2'b00) ? x_r1out : (x_bypass_logic1 == 2'b01) ? m_addr : (x_bypass_logic1 == 2'b10) ? i_wdata : 16'b0;
+
+    assign alu_in1 = (x_bypass_logic1 == 2'b00) ? sign_ext : (x_bypass_logic1 == 2'b01) ? m_addr : (x_bypass_logic1 == 2'b10) ? i_wdata : 16'b0;
+
+    wire [15:0] x_r2out_new = sign_ext;
+
+
+    // ALU // used o_reult instead of x_alu_out
+    lc4_alu a0 (.i_insn(x_insn), .i_pc (x_pc), .i_r1data(alu_in1), .i_r2data(alu_in2), .o_result(o_result));     
+
+
+
+
+
+
+
+
+
+
+
+
+    //memory phase
+    wire [15:0] m_pc;
+    wire [15:0] m_insn;
+    wire [15:0] m_addr;
+    wire [15:0] m_mem_to_write;
+    wire [2:0] m_r2sel;
+    wire [2:0] m_wsel;
+    wire m_regfile_we;
+    wire m_nzp_we;
+    wire m_is_store;
+    wire m_is_load;
+    wire [1:0] m_stall;
+
+   
+    
+
+
+
 
 
 
@@ -169,5 +300,29 @@ module branch(input  wire [15:0] pc_plus_one,
       assign mux6_in = (match_out & is_branch) | (is_control_insn);
 
       assign next_pc = (mux6_in == 3'd0) ? pc_plus_one : o_result;
+
+endmodule
+
+
+module x_bypass_l(input  wire [2:0] x_rsel,
+            input  wire [2:0] m_wsel,
+            input  wire [2:0] w_wsel,
+            input  wire m_regfile_we,
+            input  wire w_regfile_we,
+            output wire [1:0] x_bypass_logic);
+
+      /*** YOUR CODE HERE ***/
+
+      wire w1, w2, w3, w4;
+
+      assign w1 = (x_rsel == m_wsel) ?  1'b1 : 1'b0;
+
+      assign w2 = (x_rsel == w_wsel) ?  1'b1 : 1'b0;
+
+      assign w3 = w1 & m_regfile_we;
+
+      assign w4 = w2 & w_regfile_we;
+
+      assign x_bypass_logic = ((w3 == 1'b0) & (w4 ==1'b0)) ? 2'b00 : ((w3 == 1'b0) & (w4 ==1'b1)) ? 2'b01 : ((w3 == 1'b1) & (w4 ==1'b0)) ? 2'b10 : 2'b11;
 
 endmodule
